2a328bc4e92730232efb47620b2beba5baf431d1
finn4990 <45741636+finn4990@users.noreply.github.com>
2026-09-11 15:21:11 +0200

    Add files via upload

---
+++ back-end/src/main/java/com/project/code/Repo/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.project.code.Repo;
+
+import com.project.code.Model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Customer findByEmail(String email);
+    
+    Customer findByid(Long id);
+}
+
+
---
+++ back-end/src/main/java/com/project/code/Repo/OrderDetailsRepository.java
@@ -0,0 +1,9 @@
+package com.project.code.Repo;
+
+import com.project.code.Model.OrderDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Long> {
+}
